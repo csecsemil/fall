63df374d71eea8964354a1fcb7319943efc473f3
csecsemil <csecs.emil2009@gmail.com>
2025-10-24 10:15:13 +0200

    Added Pictures

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 let score = 0; 
+const scoreElement = document.getElementById('pontszám');
+
+const LETTER_PICTURES = [
+    'file:///home/emil/hackclub/fall/fal_2.png',
+    'file:///home/emil/hackclub/fall/fal_1.png',
+    'file:///home/emil/hackclub/fall/fal_3.png'
+];
+
+class Leaf {
+    constructor() {
+
+    }
+}
