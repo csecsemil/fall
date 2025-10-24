ad1d1c4d6cd6d62b7762bd71c6b8d26c4a89a216
csecsemil <csecs.emil2009@gmail.com>
2025-10-24 14:04:51 +0200

    falling actualli letters

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let score = 0;
 const scoreElement = document.getElementById('pontszám');
 
 const LETTER_PICTURES = [
-    'file:///home/emil/hackclub/fall/fal_2.png',
-    'file:///home/emil/hackclub/fall/fal_1.png',
-    'file:///home/emil/hackclub/fall/fal_3.png'
+    'fal_2.png',
+    'fal_1.png',
+    'fal_3.png'
 ];
 
 class Leaf {
     constructor() {
-     this.imageURL = LETTER_PICTURES[Math.foor(Math.random() * LETTER_PICTURES.lenght)];
+     this.imageURL = LETTER_PICTURES[Math.floor(Math.random() * LETTER_PICTURES.length)];
     
      this.element = document.createElement('div');
      this.element.className = 'leaf-element';
@@ -19,7 +19,6 @@ class Leaf {
      this.imageElement.alt = "Falling Leaf"
 
      this.element.appendChild(this.imageElement)
-
 
      this.element.onclick = () => {
         score++; // Increment the score
@@ -32,7 +31,6 @@ class Leaf {
         }, 100)
      };
 
-
      document.body.appendChild(this.element); //hozza adas az oldalra
 
      //Inicializálja az összes paramétert
@@ -41,18 +39,17 @@ class Leaf {
 
     reset() {
         //random meret
-        this.size Math.ramdom() * 40 + 50;
-        this.element.style.width = '${this.size}px';
-        this.element.style.height = '${this.size}px';
+        this.size = Math.random() * 15 + 10;
+        this.element.style.width = `${this.size}px`;
+        this.element.style.height = `${this.size}px`;
 
         //indulás a képernyő tetején kevul
         this.x = Math.random() * window.innerWidth;
         this.y = -this.size - (Math.random() * window.innerHeight)
 
-        this.speed = Math.ramdom() *1.36 +1;
+        this.speed = Math.random() * 1.36 + 1;
 
         this.updateDOM();
-
     }
 
     update() {
@@ -65,15 +62,35 @@ class Leaf {
     }
 
     updateDOM() {
-        this.element.style.left = '${this.x}px';
-        this.element.style.top = '${this.y}px';
+        this.element.style.left = `${this.x}px`;
+        this.element.style.top = `${this.y}px`;
     }
-    
-    const leaves = [];
-    const LEAF_COUNT = 5;
-
-    function initialze() {
-        
-    }
-
 }
+
+const leaves = [];
+const LEAF_COUNT = 5;
+
+function initialize() {
+    for (let i = 0; i < LEAF_COUNT; i++) {
+        if (LETTER_PICTURES.length > 0) {
+            leaves.push(new Leaf());
+        }
+    }
+}
+
+function gameLoop() {
+    leaves.forEach(leaf => {
+        leaf.update();  //uj kordinata
+        leaf.updateDOM();
+    });
+
+    requestAnimationFrame(gameLoop);
+}
+
+window.onload = () => {
+    initialize();
+    gameLoop();
+};
+
+window.addEventListener('resize', () => {
+})
